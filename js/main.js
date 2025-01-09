de24e6f42a36eb03672b9dba0a27dfe95f475529
Salim Raboudi <salim.7777@live.fr>
2025-01-09 04:25:40 +0100

    tracing the key elements of the template to start fresh

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Salim RABOUDI." ,"Engineer.", "Programmer.", "Developer."],
       typeSpeed: 100,
       loop: true,
     });
